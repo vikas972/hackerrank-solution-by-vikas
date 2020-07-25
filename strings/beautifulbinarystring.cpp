9b9e1764c1a553e453521be105b35f9e6bf39e14
vikas972 <40334523+vikas972@users.noreply.github.com>
2020-07-25 10:36:40 +0530

    Create beautifulbinarystring.cpp

---
+++ strings/beautifulbinarystring.cpp
@@ -0,0 +1,54 @@
+//https://www.hackerrank.com/challenges/beautiful-binary-string/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the beautifulBinaryString function below.
+int beautifulBinaryString(string b) {
+    string pat="010";
+    int M = pat.length(); 
+    int N = b.length(); 
+    int res = 0; 
+    
+    /* A loop to slide pat[] one by one */
+    for (int i = 0; i <= N - M; i++) 
+    {  
+        /* For current index i, check for  
+           pattern match */
+        int j; 
+        for (j = 0; j < M; j++) 
+            if (b[i+j] != pat[j]) 
+                break; 
+   
+        // if pat[0...M-1] = txt[i, i+1, ...i+M-1] 
+        if (j == M)   
+        { 
+           res++; 
+           j = 0; 
+           i=i+M-1;
+        } 
+    } 
+
+    return res;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string b;
+    getline(cin, b);
+
+    int result = beautifulBinaryString(b);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
